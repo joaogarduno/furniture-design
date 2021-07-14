42594db290b4adb7621764843d2cfd7f09fd1d10
Joao Garduño <joao.garduno@gmail.com>
2021-07-14 01:29:01 -0500

    Estructura 9 -> Rama navMenu 1 - Modificaciones al navMenu, detalles de estilos, html y javascript, parte 1

---
+++ js/navMenu.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+    const burger = document.querySelector('.nav__burger');
+    const nav = document.querySelector('.nav__ul')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
